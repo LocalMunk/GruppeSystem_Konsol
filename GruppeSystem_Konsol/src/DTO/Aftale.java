2d06d391c56bab31ba0eb3873919e6048cc945bb
frederik <bgbg>
2017-04-11 10:52:24 +0200

    dataklasser v3 ???

---
+++ GruppeSystem_Konsol/src/DTO/Aftale.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+import brugerautorisation.data.Bruger;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author frederik
+ */
+public class Aftale{
+    
+    private String tidspunkt;
+    private String lokation;
+    private String id;
+    private String navn;
+    private String beskrivelse;
+    private ArrayList<Bruger> ansvarlige;
+    
+    public Aftale() {
+    
+           
+        
+    }
+    
+    
+}
